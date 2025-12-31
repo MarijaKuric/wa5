41680d08333ff107c181a4652eb9bd8b5fcd33e1
MarijaKuric <159777612+MarijaKuric@users.noreply.github.com>
2025-12-31 14:31:39 +0100

    treci commit
    Refactor database connection setup and improve code readability.

--- pizza-express/db.js
+++ pizza-express/db.js
@@ -3,16 +3,14 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-// Čitanje podataka iz .env
 const username = process.env.MONGO_USERNAME;
 const password = process.env.MONGO_PASSWORD;
 const cluster = process.env.MONGO_CLUSTER;
 const dbName = process.env.MONGO_DB_NAME;
 
-// Kreiranje URI-ja za MongoDB
 const mongoURI = `mongodb+srv://${username}:${password}@${cluster}/?retryWrites=true&w=majority`;
 
-let client; // čuvamo instancu klijenta da se ne spajamo više puta
+let client;
 
 async function connectToDatabase() {
   try {
